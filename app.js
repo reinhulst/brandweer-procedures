ad8f2682daccaae6a02b27e4a1786542a908bd94
reinhulst <reinhulst@gmail.com>
2025-12-08 21:08:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ const procedures = [
     description: "Infographics.",
     url: "https://1drv.ms/b/c/1429f39337a31c60/ERz5iu_fTz5HrYTJ66-1X2QB0pBHvWKR9R6twEMGEmU2Vw?e=Lgu1hC"
   }
-
-
   // Voeg hier meer items toe
 ];
 
